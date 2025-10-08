cac9921166c5b0af582c4121bf0a48bf96f2ecb6
SAIF Ali <41753389+saifali7@users.noreply.github.com>
2025-10-08 13:32:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors({
       'http://127.0.0.1:3000',
       'http://localhost:5500',
       'http://127.0.0.1:5500',
-      'alishanapp.netlify.app',
+      'https://alishanapp.netlify.app',
       /\.netlify\.app$/,
       /\.onrender\.com$/
     ];
